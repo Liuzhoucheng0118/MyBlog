e2efca5175570a68d87af0fc69ff8af1a6a49abc
lzc <369039150@qq.com>
2021-12-01 20:42:54 +0800

    友链修复

--- src/main/java/com/lzc/blog/controller/FriendController.java
+++ src/main/java/com/lzc/blog/controller/FriendController.java
@@ -1,12 +1,17 @@
 package com.lzc.blog.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.lzc.blog.pojo.Friend;
 import com.lzc.blog.service.FriendService;
 import com.lzc.blog.service.InfoService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,11 +38,42 @@ public class FriendController {
         return "/friends";
     }
 
-
+    @ApiOperation(value = "申请成为友链")
     @PostMapping("/apply")
     public String apply(Friend friend, Model model) {
         friendService.save(friend);
         model.addAttribute("message", "申请成功");
         return "/friends";
     }
+
+    @ApiOperation(value = "获取全部的友链信息")
+    @ApiImplicitParam(name = "page",value = "第几页友链")
+    @GetMapping("/admin/friends")
+    public String getImages(@RequestParam(value = "page",defaultValue = "1")Integer page, Model model){
+        Page<Friend> pages = new Page(page,7);
+        IPage<Friend> friends = friendService.getAllFriend(pages);
+        model.addAttribute("pages",friends);
+        return "/admin/friends";
+    }
+
+    @ApiOperation(value = "激活用户")
+    @RequestMapping("/admin/friends/{id}/active")
+    public String toActiveUser(@PathVariable(value = "id") Integer id){
+        friendService.updateShow(id);
+        return "redirect:/admin/friends";
+    }
+    @ApiOperation(value = "关闭用户")
+    @RequestMapping("/admin/friends/{id}/close")
+    public String toCloseUser(@PathVariable(value = "id") Integer id){
+        friendService.toCloaseFriend(id);
+        return "redirect:/admin/friends";
+    }
+    @ApiOperation(value = "删除用户")
+    @RequestMapping("/admin/friends/{id}/delete")
+    public String toDeleteUser(@PathVariable(value = "id") Integer id){
+        friendService.deleteFriendById(id);
+        return "redirect:/admin/friends";
+    }
+
+
 }
