fcca2c6751160783e0e3b032efecba92a24d042c
lzc <369039150@qq.com>
2021-12-01 20:42:54 +0800

    2021年12月1日 20:42:16 加了前端的文章图片下载

--- src/main/java/com/lzc/blog/controller/FriendController.java
+++ src/main/java/com/lzc/blog/controller/FriendController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -24,12 +25,12 @@ public class FriendController {
     @Autowired
     private InfoService infoService;
 
-    @PostMapping("/friends")
+    @GetMapping("/friends")
     public String toFriends(Model model) {
 
         List<Friend> showFriend = friendService.getShowFriend();
         model.addAttribute("friends", showFriend);
-        return "friends";
+        return "/friends";
     }
 
 
@@ -37,6 +38,6 @@ public class FriendController {
     public String apply(Friend friend, Model model) {
         friendService.save(friend);
         model.addAttribute("message", "申请成功");
-        return "friends";
+        return "/friends";
     }
 }
